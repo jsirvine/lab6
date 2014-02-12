78445a04c73afd238cae202d007a7c1605be6728
Jamie Irvine <jirvine@cs.stanford.edu>
2014-02-12 15:08:01 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,31 +2,42 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+    $('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+    $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+    // Prevent following the link
+    e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+    // Get the div ID, e.g., "project3"
+    var projectID = $(this).closest('.project').attr('id');
+    // get rid of 'project' from the front of the id 'project3'
+    var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+    $.get("/project/" + idNumber, callback);
+
+    console.log("/project/" + idNumber);
+}
+
+function callback(results) {
+    console.log(results);
+    var htmlStr = "<img class=detailsImage src=\"" + results.image + "\">"
+        + "<h4>" + results.title + "</h4></div>"
+        + "<h5>" + results.date + "</h5>"
+        + "<p>" + results.summary + "</p>";
+    $("#project" + results.id + " .details").html(htmlStr);
 }
 
 /*
@@ -34,5 +45,18 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+    console.log("User clicked on color button");
+    $.get("/palette", colorCallback);
+}
+
+function colorCallback(results) {
+    console.log(results);
+    var colors = results.colors.hex;
+    console.log(colors);
+    // Change colors
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
 }
